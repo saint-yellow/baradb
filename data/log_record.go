d8df6c0d987c1466666ebdf82a8e86825d541349
Saint-Yellow Huang Jionghui <saint-yellow@hotmail.com>
2023-06-10 07:51:15 +0800

    test(log_record): implement and test the process of encoding/decoding a log record

--- data/log_record.go
+++ data/log_record.go
@@ -1,6 +1,9 @@
 package data
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"hash/crc32"
+)
 
 // LogRecordPosition: index of in-memory data
 type LogRecordPosition struct {
@@ -11,8 +14,8 @@ type LogRecordPosition struct {
 type LogRecordType = byte
 
 const (
-	NormalLogRecord  LogRecordType = iota // NormalLogRecord indicates that a log record is normal to read, update or delete
-	DeletedLogRecord                      // DeletedLogRecord indicates that a log record is deleted and unusable
+	NormalLogRecord  LogRecordType = iota + 1 // NormalLogRecord indicates that a log record is normal to read, update or delete
+	DeletedLogRecord                          // DeletedLogRecord indicates that a log record is deleted and unusable
 )
 
 // maxLogRecordHeaderSize Maximum size of a header of a log record
@@ -35,15 +38,97 @@ type logRecordHeader struct {
 
 // EncodeLogRecord encodes a log record
 func EncodeLogRecord(lr *LogRecord) ([]byte, int64) {
-	return nil, 0
+	// Initialize a byte array of the header
+	header := make([]byte, maxLogRecordHeaderSize)
+
+	// Store the type of the log record to the header
+	header[4] = lr.Type
+
+	var index int = 5
+	// Store the size of the key and the value of the log record to the header
+	index += binary.PutVarint(header[index:], int64(len(lr.Key)))
+	index += binary.PutVarint(header[index:], int64(len(lr.Value)))
+
+	// Total size of the header and the log reocrd
+	var size int = index + len(lr.Key) + len(lr.Value)
+
+	// Copy the known data to the new byte array
+	encodedBytes := make([]byte, size)
+	copy(encodedBytes[:index], header[:index])
+	copy(encodedBytes[index:], lr.Key)
+	copy(encodedBytes[index+len(lr.Key):], lr.Value)
+
+	// Do CRC for the header and the log record
+	crc := crc32.ChecksumIEEE(encodedBytes[4:])
+	binary.LittleEndian.PutUint32(encodedBytes[:4], crc)
+
+	return encodedBytes, int64(size)
+}
+
+// encodeLogRecordHeader returns the encodeed log record header and its length
+func encodeLogRecordHeader(lr *LogRecord) ([]byte, int64) {
+	// Initialize a byte array of the header
+	header := make([]byte, maxLogRecordHeaderSize)
+
+	// Store the type of the log record to the header
+	header[4] = lr.Type
+
+	var index int = 5
+	// Store the size of the key and the value of the log record to the header
+	index += binary.PutVarint(header[index:], int64(len(lr.Key)))
+	index += binary.PutVarint(header[index:], int64(len(lr.Value)))
+
+	// Total size of the header and the log reocrd
+	var size int = index + len(lr.Key) + len(lr.Value)
+
+	// Copy the known data to the new byte array
+	encodedBytes := make([]byte, size)
+	copy(encodedBytes[:index], header[:index])
+	copy(encodedBytes[index:], lr.Key)
+	copy(encodedBytes[index+len(lr.Key):], lr.Value)
+
+	// Do CRC for the header and the log record
+	crc := crc32.ChecksumIEEE(encodedBytes[4:])
+	binary.LittleEndian.PutUint32(encodedBytes[:4], crc)
+
+	return encodedBytes[:index], int64(index)
 }
 
 // decodeLogRecordHeader Decode a header of a log record
 func decodeLogRecordHeader(buffer []byte) (*logRecordHeader, int64) {
-	return nil, 0
+	if len(buffer) <= 4 {
+		return nil, 0
+	}
+
+	header := &logRecordHeader{
+		crc:           binary.LittleEndian.Uint32(buffer[:4]),
+		logRecordType: buffer[4],
+	}
+
+	var index int = 5
+
+	// Get the actual size of the key of the log reocrd
+	keySize, n := binary.Varint(buffer[index:])
+	header.keySize = uint32(keySize)
+	index += n
+
+	// Get the actual size of the value of the log record
+	valueSize, n := binary.Varint(buffer[index:])
+	header.valueSize = uint32(valueSize)
+	index += n
+
+	return header, int64(index)
 }
 
 // crc Get the CRC value of a log record
 func (lr *LogRecord) crc(header []byte) uint32 {
-	return 0
+	if lr == nil {
+		return 0
+	}
+
+	crc := crc32.ChecksumIEEE(header[:])
+	crc = crc32.Update(crc, crc32.IEEETable, lr.Key)
+	crc = crc32.Update(crc, crc32.IEEETable, lr.Value)
+
+	return crc
 }
---
+++ data/log_record_test.go
@@ -0,0 +1,65 @@
+package data
+
+import (
+	"hash/crc32"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	// Samples: 1. Normal; 2. Nil value; 3. Deleted
+	samples = [3]*LogRecord{
+		{
+			Key:   []byte("114"),
+			Value: []byte("514"),
+			Type:  NormalLogRecord,
+		},
+		{
+			Key:   []byte("191810"),
+			Value: nil,
+			Type:  NormalLogRecord,
+		},
+		{
+			Key:   []byte("114514"),
+			Value: []byte("1919810"),
+			Type:  DeletedLogRecord,
+		},
+	}
+)
+
+func TestEncodingLogRecord(t *testing.T) {
+	for _, lr := range samples {
+		b, n := EncodeLogRecord(lr)
+		assert.NotNil(t, b)
+		assert.Greater(t, n, int64(5))
+	}
+}
+
+func TestEncodingLogRecordHeader(t *testing.T) {
+	for _, lr := range samples {
+		b1, _ := encodeLogRecordHeader(lr)
+		assert.Equal(t, uint8(lr.Type), b1[len(b1)-3])
+		b2, _ := EncodeLogRecord(lr)
+		assert.EqualValues(t, b1, b2[:len(b1)])
+	}
+}
+
+func TestDecodingLogRecordHeader(t *testing.T) {
+	for _, lr := range samples {
+		b, _ := encodeLogRecordHeader(lr)
+		lrh, _ := decodeLogRecordHeader(b)
+		assert.Equal(t, lrh.logRecordType, lr.Type)
+		assert.Equal(t, int(lrh.keySize), len(lr.Key))
+		assert.Equal(t, int(lrh.valueSize), len(lr.Value))
+	}
+}
+
+func TestLogRecordCRC(t *testing.T) {
+	for _, lr := range samples {
+		b, _ := encodeLogRecordHeader(lr)
+		crcValue := lr.crc(b[crc32.Size:])
+		h, _ := decodeLogRecordHeader(b)
+		assert.Equal(t, crcValue, h.crc)
+	}
+}
