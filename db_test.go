f0ba9782e19b93aedd99f8fcafbc2cb28e9f145b
Saint-Yellow Huang Jionghui <saint-yellow@hotmail.com>
2023-06-13 00:38:28 +0800

    test(db): add a teardown method to clear data files

--- db_test.go
+++ db_test.go
@@ -1,25 +1,39 @@
 package baradb
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func destroyDB(db *DB) {
+	if db != nil {
+		db.activeFile.Close()
+		err := os.RemoveAll(db.options.Directory)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
 func TestDB_Launch(t *testing.T) {
 	db, err := LaunchDB(DefaultOptions)
+	defer destroyDB(db)
 	assert.Nil(t, err)
 	assert.NotNil(t, db)
 }
 
 func TestDB_Put(t *testing.T) {
 	db, _ := LaunchDB(DefaultOptions)
+	defer destroyDB(db)
 	err := db.Put([]byte("114"), []byte("514"))
 	assert.Nil(t, err)
 }
 
 func TestDB_Get(t *testing.T) {
 	db, _ := LaunchDB(DefaultOptions)
+	defer destroyDB(db)
 	db.Put([]byte("114"), []byte("514"))
 	b, err := db.Get([]byte("114"))
 	assert.Nil(t, err)
@@ -28,6 +42,7 @@ func TestDB_Get(t *testing.T) {
 
 func TestDB_Delete(t *testing.T) {
 	db, _ := LaunchDB(DefaultOptions)
+	defer destroyDB(db)
 	db.Put([]byte("114"), []byte("514"))
 	err := db.Delete([]byte("114"))
 	assert.Nil(t, nil)
